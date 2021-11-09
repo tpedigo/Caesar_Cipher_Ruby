b96a7637d86203ec23021759718eaabe8c67e673
Tara Pedigo <tkirkhus@gmail.com>
2021-11-09 09:22:10 -0500

    Fix method to accept non-letter characters and non-string input

--- script.rb
+++ script.rb
@@ -1,25 +1,32 @@
 def caesar_cipher(string, shift)
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
+    string = string.to_s
     split_string = string.split("")
     ciphered_string = ""
-    split_string.map { |letter|
-        if letter.downcase == letter
-            letter_index = alphabet.index(letter)
-            if (letter_index + shift) > 25
-                letter_index = (letter_index + shift) - 26
-                ciphered_string += alphabet[letter_index]
+    split_string.map { |character|
+        if character.downcase == character
+            # First checks if character is a letter
+            if alphabet.include? character
+                character_index = alphabet.index(character)
+                if (character_index + shift) > 25 
+                    # wraps back to "a" if necessary
+                    character_index = (character_index + shift) - 26
+                    ciphered_string += alphabet[character_index]
+                else
+                    ciphered_string += alphabet[character_index + shift]
+                end
             else
-                ciphered_string += alphabet[letter_index + shift]
+                ciphered_string += character
             end
-        else
-            upcase_alphabet = alphabet.map {|letter| letter.upcase}
-            letter_index = upcase_alphabet.index(letter)
-            if (letter_index + shift) > 25
-                letter_index = (letter_index + shift) - 26
-                ciphered_string += upcase_alphabet[letter_index]
+        else # In the case of uppercase letters
+            upcase_alphabet = alphabet.map {|character| character.upcase}
+            character_index = upcase_alphabet.index(character)
+            if (character_index + shift) > 25
+                character_index = (character_index + shift) - 26
+                ciphered_string += upcase_alphabet[character_index]
             else
-                ciphered_string += upcase_alphabet[letter_index + shift]
+                ciphered_string += upcase_alphabet[character_index + shift]
             end
         end
     }
